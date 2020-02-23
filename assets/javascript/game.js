f510f840dbb0d83e3c7f02c3d818ddbbc80c42f9
Dylan Ambrose <djambro44@gmail.com>
2020-02-23 13:34:10 -0700

    added psuedo code to JS

---
+++ assets/javascript/game.js
@@ -0,0 +1,38 @@
+//define variables
+//  randomNumer
+//  buttonOneValue
+//  buttonTwoValue
+//  buttonThreeValue
+//  buttonFourValue
+//  totalScore
+//  wins
+//  losses
+
+//functions 
+
+// gameStart
+//      a)clear wins/losses
+//      b)calls getRandomNum and all button values functions
+
+//  getRandomNum between 19-120
+
+//  getButton1Value
+
+//  getButton2Value
+
+//  getButton3Value
+
+//  getButton4Value
+
+//  getTotalScoreValue
+
+// get wins
+
+//  get losses
+
+//  game
+
+// if totalscore === randomNum
+//          wins++
+//else if (totalScore > randomNum)
+//          losses++
